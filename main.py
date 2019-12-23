c7a2fedd401c99a69e0c9a29a0103ff03d200291
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2019-12-23 20:55:38 +0700

    🚸 v0.4.0
    💄 Better looking chart
    🚸 Better average level format

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
     `nihongo.py`
 '''
 
+from math import ceil
+from math import floor
 from pygal.style import CleanStyle
 
+import os
 import pygal
 import sqlite3
 
@@ -18,7 +21,7 @@ def main():
     print('- Flashcard Statistics -')
     print()
     print('Total: {}'.format(len(rows)))
-    print('Average: {:.0f}-{:.2f}'.format(average(data)//3 + 1, average(data) % 3))
+    print('Average: {:.0f}-{:.0f} (+{:.2f})'.format(average(data) // 3 + 1, floor(average(data) % 3), average(data) % 3))
     print()
 
     for i in range(len(data) - 1, -1, -1):
@@ -39,8 +42,7 @@ def create_connection(db_file):
 
 def select_progress(conn):
     cur = conn.cursor()
-    cur.execute(
-        "SELECT ZPROGRESS, Z8_BECAMEACTIVEVIAFLASHCARDPACK FROM ZFLASHCARD")
+    cur.execute('SELECT ZPROGRESS, Z8_BECAMEACTIVEVIAFLASHCARDPACK FROM ZFLASHCARD')
 
     rows = [i[0] for i in cur.fetchall() if i[1] != None]
 
@@ -54,16 +56,60 @@ def average(data):
 def render(data):
     chart = pygal.Bar()
 
-    # for i in range(0, max(data) + 1):
-    #     chart.add('{}-{}'.format(i//3 + 1, i % 3), data.count(i))
-
-    chart.style = CleanStyle
-
+    # Chart Data
+    # for i in range(0, ceil(len(data)/3)):
+    #     chart.add('Level {}'.format(i), data[i*3: i*3 + 3])
     chart.add('Learned Words', data)
 
-    chart.x_labels = ['{}-{}'.format(i//3 + 1, i % 3) for i in range(len(data))]
+    # Chart Titles
+    chart.title = 'Learned Words by Level'
 
+    # Chart Data
+    chart.x_labels = ['{}-{}'.format(i//3 + 1, i % 3)
+                      for i in range(len(data))]
+    chart.y_labels = calculate_y_labels(min(data), max(data))
+    
+    # Chart Legends
+    chart.show_legend = False
+    chart.legend_at_bottom = True
+    chart.legend_at_bottom_columns = 5
+    chart.legend_box_size = 16
+
+    # Chart Render
+    chart.style = CleanStyle
     chart.render_to_file('chart.svg')
+
+    try:
+        os.system('open chart.svg')
+    except (FileNotFoundError, OSError, PermissionError):
+        print('[ERROR] Something unexpected happened, please try again.')
+    return
+
+
+def calculate_y_labels(data_min, data_max, allow_float=False, max_y_labels=15):
+    ''' Function: Calculate y labels '''
+    data_min = floor(data_min)
+    data_max = ceil(data_max)
+    
+    if allow_float:
+        data_min *= 100
+        data_max *= 100
+
+    preset = 1, 2, 5
+    data_range = list(range(0, data_min - 1, -1)) + list(range(0, data_max + 1, 1))
+    i = 0
+
+    while len(data_range) > max_y_labels:
+        data_range = list(range(0, data_min - preset[i % 3] * 10**(i // 3), -1 * preset[i % 3] * 10**(i // 3)))
+        data_range += list(range(0, data_max + preset[i % 3] * 10**(i // 3), preset[i % 3] * 10**(i // 3)))
+        i += 1
+        
+    data_range.sort()
+
+    if allow_float:
+        data_range = [i/100 for i in data_range]
+
+    return data_range
 
 
 main()
